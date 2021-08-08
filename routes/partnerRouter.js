bf92669ca82b45b648c9f5d4958f6b3a707f1b63
Roythewolf <74839058+Roythewolf@users.noreply.github.com>
2021-08-08 19:51:33 -0400

    workshop-3-update
    First Repository

---
+++ routes/partnerRouter.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const partnerRouter = express.Router();
+const Partner = require('../models/partner')
+const authenticate = require('../authenticate')
+
+partnerRouter
+  .route("/")
+  .get((req, res, next) => {
+    Partner.find()
+      .then((partners) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(partners);
+      })
+      .catch((err) => next(err));
+  })
+  //need admin//
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Partner.create(req.body)
+      .then((partner) => {
+        console.log("Partner Created ", partner);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(partner);
+      })
+      .catch((err) => next(err));
+  })
+  .put(authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /partners");
+  })
+  //need admin//
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+    Partner.deleteMany()
+      .then((response) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(response);
+      })
+      .catch((err) => next(err));
+  });
+
+
+partnerRouter
+  .route("/:partnerId")
+  .get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+      .then((partner) => {
+        if (partner) {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(partner);
+        } else {
+          err = new Error(`Partner ${req.params.partnerId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .post(authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end(`post operation not supported on /partner/${req.params.partnerId}`);
+  })
+//need admin//
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    Partner.findByIdAndUpdate(
+      req.params.partnerId,
+      { $set: req.body },
+      { new: true }
+    )
+      .then((partner) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(partner);
+      })
+      .catch((err) => next(err));
+  })
+  //need admin//
+  .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Partner.findById(req.params.partnerId)
+      .then((partner) => {
+        if (partner) {
+          for (let i = partner.length - 1; i >= 0; i--) {
+            partner.id(partner[i]._id).remove();
+          }
+          partner
+            .save()
+            .then((partner) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(partner);
+            })
+            .catch((err) => next(err));
+        } else {
+          err = new Error(`Partner ${req.params.partnerId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  });
+
+module.exports = partnerRouter;
